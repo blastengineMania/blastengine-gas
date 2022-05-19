dce3382188edc0c06c6d427e98a701d2ef7eeffe
Atsushi <atsushi@moongift.jp>
2022-05-19 17:36:49 +0900

    Remove ignore test

--- tests/transaction.ts
+++ tests/transaction.ts
@@ -25,32 +25,3 @@ function _transaction_attachment_test() {
 	const res = transaction.send();
 	console.log(res);
 }
-function test() {
-	const up = PropertiesService.getUserProperties();
-	const b = new BlastEngine(up.getProperty('API_USER')!, up.getProperty('API_KEY')!);
-  const metadata = {
-  "from": {
-    "email": "info@opendata.jp",
-    "name": "送信者サンプル"
-  },
-  "to": "atsushi@moongift.jp",
-  "subject": "テスト件名",
-  "text_part": "テスト配信",
-  };
-  const response = UrlFetchApp.fetch('https://www.moongift.jp/logo.jpg');
-  var form = FetchApp.createFormData(); // Create form data
-  form.append(
-    "data",
-    Utilities.newBlob(JSON.stringify(metadata), "application/json")
-  );
-  form.append("file", response.getBlob());
-  var url = "https://app.engn.jp/api/v1/deliveries/transaction";
-  b.generateToken();
-  var params = {
-    method: "POST",
-    headers: { Authorization: "Bearer " + b.token },
-    body: form,
-  };
-  var res = FetchApp.fetch(url, params);
-  Logger.log(res);
-}
